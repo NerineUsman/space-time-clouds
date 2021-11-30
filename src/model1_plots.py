f2c6cfe3280eb055c7c0c828a15880d6ecc898a5
NerineUsman <nerine@usman.nl>
2021-11-30 18:19:25 +0100

    plots model1

--- src/model1_plots.py
+++ src/model1_plots.py
@@ -8,24 +8,41 @@ Created on Mon Nov 29 11:05:38 2021
 """
 
 # import modules
-import pickle
 import os
 import matplotlib.pyplot as plt
-import xarray as xr
+import matplotlib as mpl
 import numpy as np
 import pandas as pd
 
 
-from datetime import datetime, timedelta
-
-
 # variables
 input_file = 'input_model1_local.txt'
+# input_file = './space-time-clouds/src/input_model1.txt'
 
 # functions
-def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
-        yield start_date + timedelta(n)
+def plot_distribution_next_cloud(df, title = None, nbins = 50, **kwargs):
+    fig, ax = plt.subplots(1,3,figsize = (20, 4))
+    # cth
+    ax[0].hist(df.h_t_next * 1e-3, bins = nbins, **kwargs)
+    ax[0].set_title('CTH (km)')
+    # cod
+    ax[1].hist(df.d_t_next, bins = nbins, **kwargs)
+    ax[1].set_title('COD (log($\cdot$)')
+    
+    # joint density
+    bins = [nbins, nbins]
+    h = ax[2].hist2d(df.d_t_next, df.h_t_next*1e-3, bins=bins, 
+                     cmap=plt.cm.Blues, norm=mpl.colors.LogNorm(),
+                     **kwargs)
+    ax[2].set_xlabel('COD (log($\cdot$)')
+    ax[2].set_ylabel('CTH (km)')
+    plt.colorbar(h[3], ax=ax[2])
+
+    # ax[2].set_colorbar()
+    
+    if title != None:
+        fig.suptitle(title)
+    return fig
 
 
 
@@ -35,6 +52,7 @@ if __name__ == "__main__":
         input = dict([line.split() for line in f])
     
     loc_model1_data = input['loc_model1_data']
+    loc_fig = input['loc_fig']
     
     # combine df's from all days in model 1 data
     files = [loc_model1_data + f for f in os.listdir(loc_model1_data) if (os.path.isfile(os.path.join(loc_model1_data, f)))]
@@ -68,4 +86,69 @@ if __name__ == "__main__":
     df_s = df.loc[((df.cloud == 'clear sky') | (df.cloud == 'cloud')) & (df.cloud_next == 'clear sky')  ]
 
     
+# =============================================================================
+#   plots
+# =============================================================================
 
+    # distribution for clouds after clear sky
+    title = 'Clear sky to cloud distributions'
+    fig = plot_distribution_next_cloud(df_sc, title = title, density = True )
+    fig.savefig(loc_fig + 'clear_sky_to_cloud_distr.png')
+
+    
+    # cloud to cloud overview
+    
+    fig, ax = plt.subplots(2,3,figsize = (15, 7))
+    # cth
+    ax[0,0].hist(df_cc.h_t * 1e-3, bins = 50, density = True)
+    ax[0,0].set_title('CTH (km)')
+    ax[0,1].hist(df_cc.dh * 1e-3, bins = 200, density = True)
+    ax[0,1].set_title('$\Delta$CTH (km)')
+    ax[0,2].hist(df_cc.dh * 1e-3, bins = 200, density = True)
+    ax[0,2].set_title('$\Delta$CTH (km) zoomed')
+    ax[0,2].set(xlim = [-2, 2])
+    ax[1,0].hist(df_cc.d_t, bins = 50, density = True)
+    ax[1,0].set_title('COD ($\cdot$)')
+    ax[1,1].hist(df_cc.dd, bins = 100, density = True)
+    ax[1,1].set_title('$\Delta$COD ($\cdot$)')
+    ax[1,2].hist(df_cc.dd, bins = 100, density = True)
+    ax[1,2].set_title('$\Delta$COD zoomed')
+    ax[1,2].set(xlim = [-2, 2])
+    fig.suptitle('Cloud to Cloud')
+    fig.savefig(loc_fig + 'cloud_to_cloud_overview.png')
+
+
+    
+    # cloud to cloud distribution from a few bins
+    
+    mu_h = [1e3, 6e3, 12e3] #
+    mu_d = [0, 1]
+    mu_h, mu_d = np.meshgrid(mu_h, mu_d)
+    mu_h, mu_d = [x.flatten() for x in [mu_h, mu_d]]
+    
+    delta_h = 300 # m
+    delta_d = .1
+    
+    bins = [[[h - delta_h, h + delta_h], [d - delta_d, d + delta_d]] for h,d in zip(mu_h, mu_d)]
+    
+    i = 0
+    for b in bins:       
+        print(b)
+        # filter cloud to cloud on from within bin
+        df_temp = df_cc.loc[(df_cc.h_t > b[0][0]) & (df_cc.h_t < b[0][1])
+                            & (df_cc.d_t > b[1][0]) & (df_cc.d_t < b[1][1])]
+        
+        title = f'Bin centre (h, d) = ({mu_h[i]*1e-3} km, {mu_d[i]})'
+        fig = plot_distribution_next_cloud(df_temp, title = title, density = True)
+        fig.savefig(f'{loc_fig}cloud_to_cloud_(h_d)_({mu_h[i]*1e-3}_{mu_d[i]}).png')
+        i +=1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
